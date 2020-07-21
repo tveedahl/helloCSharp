845c63834816c42c73e648063100f29767092133
Tim Veedahl <timveedahl@gmail.com>
2020-07-21 06:20:19 -0500

    Added some integer math

--- Program.cs
+++ Program.cs
@@ -6,8 +6,15 @@ namespace helloCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("The current date and time is " + DateTime.Now);
+            //Print hello world, date, and time
+            Console.WriteLine("Hello World!" + "\n");
+            Console.WriteLine("The current date and time is " + DateTime.Now + "\n");
+
+            //Do some integer math
+            int a = 18;
+            int b = 6;
+            int c = a + b;
+            Console.WriteLine(c);
         }
     }
 }
